e951023249849641161b529d44a7492e95ccda30
craftxbox <craftxbox@craftxbox.com>
2022-10-25 19:44:54 -0230

    fixes generated keys not getting the provision step

--- py/xorsetup.py
+++ py/xorsetup.py
@@ -66,47 +66,51 @@ print("YubiKey inserted!")
 if xorKeyGenerated == False:
     print("You have provided an XOR key, do you want to use automatic key provisioning? (y/n)")
     if(input() == "y"):
-        with connection:
-            session = YubiOtpSession(connection)
-            if slot is None:
-                raise ValueError("Slot must be 1 or 2")
-            if slot is SLOT.ONE:
-                print("!!!!! CONFIRM !!!!!")
-                print("This will overwrite the SLOT 1")
-                print("Slot 1 stores FACTORY PROGRAMMED (cc prefix) YubiOTP credentials (ex, cccccbcbrigfcdnudidhjcvhbkdulvvibgedbjdbljvl)")
-                print("If you have used YubiOTP on any accounts (ie, LastPass), you may lose access to those accounts!")
-                print("Some services may not accept vv prefix YubiOTP credentials. You CANNOT get the cc prefix back later!")
-                print("Do you wish to continue? y/n")
-                answer = input()
-                if answer != "y":
-                    print("ABORT")
-                    sys.exit()
+        xorKeyGenerated = True
 
+
+if xorKeyGenerated:
+    with connection:
+        session = YubiOtpSession(connection)
+        if slot is None:
+            raise ValueError("Slot must be 1 or 2")
+        if slot is SLOT.ONE:
             print("!!!!! CONFIRM !!!!!")
-            print(f"This will overwrite the SLOT {sys.argv[1]}")
+            print("This will overwrite the SLOT 1")
+            print("Slot 1 stores FACTORY PROGRAMMED (cc prefix) YubiOTP credentials (ex, cccccbcbrigfcdnudidhjcvhbkdulvvibgedbjdbljvl)")
+            print("If you have used YubiOTP on any accounts (ie, LastPass), you may lose access to those accounts!")
+            print("Some services may not accept vv prefix YubiOTP credentials. You CANNOT get the cc prefix back later!")
             print("Do you wish to continue? y/n")
             answer = input()
-            if answer == "y":
-                print("Continuing...")
-            else:
+            if answer != "y":
                 print("ABORT")
                 sys.exit()
 
-            session.put_configuration(slot, StaticPasswordSlotConfiguration(base64.a85encode(bytes.fromhex(xorKey))))
-            session.set_ndef_configuration(slot,"x-yubixor-provision://#", None, NDEF_TYPE.URI)
-            print("Loaded provision key. Remove your YubiKey and scan it with the Yubiclip-XOR app.")
-
-            didProvision = True
-        print("Continue setup? y/n")
+        print("!!!!! CONFIRM !!!!!")
+        print(f"This will overwrite the SLOT {sys.argv[1]}")
+        print("Do you wish to continue? y/n")
         answer = input()
-        if answer != "y":
+        if answer == "y":
+            print("Continuing...")
+        else:
+            print("ABORT")
             sys.exit()
 
-        waitForDevice()
+        session.put_configuration(slot, StaticPasswordSlotConfiguration(base64.a85encode(bytes.fromhex(xorKey))))
+        session.set_ndef_configuration(slot,"x-yubixor-provision://#", None, NDEF_TYPE.URI)
+        print("Loaded provision key. Remove your YubiKey and scan it with the Yubiclip-XOR app.")
 
-        connection, device, info = connect_to_device(
-            connection_types=[OtpConnection], 
-        )
+        didProvision = True
+    print("Continue setup? y/n")
+    answer = input()
+    if answer != "y":
+        sys.exit()
+
+    waitForDevice()
+
+    connection, device, info = connect_to_device(
+        connection_types=[OtpConnection], 
+    )
 
 with connection:
     session = YubiOtpSession(connection)
